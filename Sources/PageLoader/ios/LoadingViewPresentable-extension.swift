6067d4126f371c3892f00d6887eb2e8dc3662f8b
Alexander Shvets <alexander.shvets@gmail.com>
2017-11-01 17:43:51 -0400

    bump version

---
+++ Sources/PageLoader/ios/LoadingViewPresentable-extension.swift
@@ -0,0 +1,25 @@
+import UIKit
+
+// Default implementation for UIViewController.
+
+extension LoadingViewPresentable where Self: UIViewController {
+
+  public func setLoadingViewStatus(_ status: LoadingStatus, animated: Bool) {
+    if isViewLoaded {
+      switch status {
+        case .hidden:
+          // hide a custom loading view indicator on the provided view.
+          //LoadingView.hide(for: view, animated: animated)
+          print("1")
+
+        case .visible(let text):
+          // find top-most loading view and reuse it, or create a new one
+//          let loadingView = view.subviews.flatMap({ $0 as? LoadingView}).first ??
+//            LoadingView.showAdded(to: view, animated: animated)
+//          loadingView.labelText = text ?? NSLocalizedString("Loading…", comment: "")
+//          loadingView.show(animated: animated)
+                  print("2")
+      }
+    }
+  }
+}
---
+++ Sources/PageLoader/tvos/PlainSpinner.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+open class PlainSpinner: Spinner {
+    open var view: UIActivityIndicatorView
+    
+    public init(_ view: UIActivityIndicatorView) {
+        self.view = view
+    }
+    
+    open func start() {
+        view.startAnimating()
+    }
+    
+    open func stop() {
+        view.stopAnimating()
+    }
+}
+
